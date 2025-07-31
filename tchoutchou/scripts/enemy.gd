e82e8133d86b02b18d4341d5fd783ee17188c7dd
Widmo206 <widmo206@yahoo.com>
2025-07-31 09:57:10 +0100

    Added strafing behaviour to enemies
    The shooting enemies will now circle their target once they reach a certain distance

--- tchoutchou/scripts/enemy.gd
+++ tchoutchou/scripts/enemy.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 
-const BASE_HEALTH = 100.0
+const BASE_HEALTH = 10.0
 const BASE_SPEED = 50.0
-const BASE_DAMAGE = 20.0
+const BASE_DAMAGE = 1.0
 const BASE_FIRERATE = 2.0
+const DESIRED_DISTANCE = 200.0      #
+const STRAFE_SHARPNESS = 0.02       # used for the AI; don't worry about it
 
-var projectile
+
+var projectile = preload("res://scenes/bullet.tscn")
 var counter = randi_range(0, 60)
 
 var health = BASE_HEALTH
@@ -21,28 +24,32 @@ func get_direction(new_target_position: Vector2):
 
 
 func get_distance_to_target():
-	return abs((target - position).length())
+	return abs((target.position - position).length())
+
+
+func shoot():
+	var projectile_instance = projectile.instantiate()
+	projectile_instance.position = position
+	projectile_instance.rotation = direction_to_target
+	projectile_instance.damage = BASE_DAMAGE
+	get_parent().add_child(projectile_instance) # bullet is parented under the root node
 
 
 func _ready() -> void:
-	projectile = load("res://scenes/bullet.tscn")
+	target = get_node("../MainBase")
 
 
 func _physics_process(delta: float) -> void:
-	target = get_node("../MainBase").position
-	direction_to_target = get_direction(target)
-	if get_distance_to_target() > 200:
-		velocity = direction_to_target * BASE_SPEED
-	else:
-		velocity = Vector2.ZERO
-	rotation = direction_to_target.angle()
+	direction_to_target = get_direction(target.position).angle()
+	# Used desmos to find a nice equation
+	# y=-\arctan\left(S\left(x-R_{d}\right)\right)+\frac{\pi}{2}  ## angle offset as a function of distance
+	# It makes the enemy settle into a circle around the target, without managing any states or things like that
+	rotation = direction_to_target - atan(STRAFE_SHARPNESS * (get_distance_to_target() - DESIRED_DISTANCE)) + PI/2
 
-	if counter % attack_cooldown == 0:
-		var projectile_instance = projectile.instantiate()
-		projectile_instance.position = position
-		projectile_instance.rotation = direction_to_target.angle()
-		projectile_instance.damage = BASE_DAMAGE
-		get_parent().add_child(projectile_instance)
+	velocity = BASE_SPEED * Vector2.from_angle(rotation)
+
+	if counter % attack_cooldown == 0 and target.is_alive:
+		shoot()
 
 
 	counter += 1
--- tchoutchou/scripts/main_base.gd
+++ tchoutchou/scripts/main_base.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 const BASE_HEALTH = 1000.0
 
 var health = BASE_HEALTH
+var is_alive = true
 
 
 func _ready() -> void:
@@ -20,3 +21,4 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	if health <= 0:
 		get_node("Hitbox/CollisionShape2D").set_deferred("disabled", true)
 		hide()
+		is_alive = false
