5dcbde19f921bee987fe1bd4f00b0a7613c53e0c
Widmo206 <widmo206@yahoo.com>
2025-08-02 15:04:51 +0100

    bunch of stuff, see description
    * Exploders now have a trigger collider for exploding, instead of a fixed distance
    * Damage logic separated from attack detection (_on_hitbox_body_entered()) to allow for damage from Area2D's (for both MainBase and enemies)
    * cleaned up code
    * Changed scaling of enemies - instead of scaling the root node, the relevant nodes are scaled instead (not sure if that actually makes a difference, tbh)
    * enemies now spawn at double the distance from the center (1000 instead of 500)

--- tchoutchou/scripts/main_base.gd
+++ tchoutchou/scripts/main_base.gd
@@ -13,6 +13,17 @@ func _ready() -> void:
 	pass
 
 
+func process_hit(attack: Node2D):
+	#play_damage_sound = true
+	health -= attack.damage
+	attack.targets_hit += 1
+	if health <= 0:
+		get_node("Hitbox/CollisionShape2D").set_deferred("disabled", true)
+		get_node("PhysicsCollider").set_deferred("disabled", true)
+		hide()
+		is_alive = false
+
+
 func _process(delta: float) -> void:
 	$Healthbar.text = str(health)
 	#if play_damage_sound:
@@ -23,14 +34,9 @@ func _process(delta: float) -> void:
 
 
 func _on_hitbox_body_entered(attack: Node2D) -> void:
-	if not attack.hits_allies:
-		return
+	if attack.hits_allies:
+		process_hit(attack)
 
-	#play_damage_sound = true
-	health -= attack.damage
-	attack.targets_hit += 1
-	if health <= 0:
-		get_node("Hitbox/CollisionShape2D").set_deferred("disabled", true)
-		get_node("PhysicsCollider").set_deferred("disabled", true)
-		hide()
-		is_alive = false
+func _on_hitbox_area_entered(attack: Node2D) -> void:
+	if attack.hits_allies:
+		process_hit(attack)
