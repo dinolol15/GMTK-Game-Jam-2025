17049a5fb5493c19d24b6c64a46e2f122a3fbdd4
Romcodewasunavailable <120272499+Romcodewasunavailable@users.noreply.github.com>
2025-08-03 19:17:45 +0200

    debug mode button

--- tchoutchou/scripts/locomotive.gd
+++ tchoutchou/scripts/locomotive.gd
@@ -1,7 +1,7 @@
 class_name Locomotive
 extends PathFollow2D
 
-@export var speed := 300.0
+@export var speed := 100.0
 @export var wagon_separation := 72.0
 @export var is_active := false:
 	set(value):
